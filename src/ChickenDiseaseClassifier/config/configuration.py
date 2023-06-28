4b0fa30f8fcd4e3558bfae4dea6cf64f3e847f4c
Chizzle_001 <chirag1346@gmail.com>
2023-06-28 21:34:48 +0530

    prepare base model completed

--- src/ChickenDiseaseClassifier/config/configuration.py
+++ src/ChickenDiseaseClassifier/config/configuration.py
@@ -1,8 +1,8 @@
 from ChickenDiseaseClassifier.constants import *
 from ChickenDiseaseClassifier.utils.common import read_yaml,create_directories
-from ChickenDiseaseClassifier.entity.config_entity import DataIngestionConfig
+from ChickenDiseaseClassifier.entity.config_entity import DataIngestionConfig,PrepareBaseModelConfig
 
-class ConfiguartionManager:
+class ConfigurationManager:
     def __init__(
         self,
         config_filepath = CONFIG_FILE_PATH,
@@ -30,4 +30,22 @@ class ConfiguartionManager:
         )
         
         return data_ingestion_config
- 
+    
+    
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        
+        create_directories([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+
+        return prepare_base_model_config
