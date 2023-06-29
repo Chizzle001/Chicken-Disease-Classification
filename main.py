d15a006e8c9bd97d7dd482c72ef1e6d2d809c80a
Chizzle_001 <chirag1346@gmail.com>
2023-06-29 18:57:00 +0530

    traning added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from ChickenDiseaseClassifier import logger
 from ChickenDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from ChickenDiseaseClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-
+from ChickenDiseaseClassifier.pipeline.stage_03_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 if __name__ == '__main__':
@@ -27,4 +27,14 @@ except Exception as e:
         raise e
 
 
-    
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+   
